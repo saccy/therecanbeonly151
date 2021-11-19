630b0e0a11cd9868aa88d6c75e22d79abf6a3f24
Andrew Hudson <a.j.hudson@hotmail.com>
2021-11-19 23:07:08 +1100

    debugging cookies

--- therecanbeonly151/src/components/Pokemans.vue
+++ therecanbeonly151/src/components/Pokemans.vue
@@ -8,87 +8,105 @@
       
       <div class="columns">
         <div class="column is-half is-offset-one-quarter">
-          <!-- <h1 class="title">Original pokemans is best pokemans</h1> -->
           <h2 class="subtitle">Gotta name 'em all in <b>3 minutes</b>!</h2>
           <h2 class="subtitle">A dimishing points multiplier is in effect, the quicker you are the better your score.</h2>
         </div>
       </div>
       
-      <div class="columns">
-        <div class="column is-half is-offset-one-quarter">
-          <!-- <button class="button is-primary is-fullwidth" @click="timerEnabled(300)">Start</button> -->
-          <button v-show="!timerEnabled" class="button is-primary is-fullwidth" @click="setTimer(180)">Start</button>
-          <button v-show="timerEnabled" class="button is-danger is-fullwidth" @click="stopTimer()">End game</button>
+{{ playerName }}
+
+      <div v-show="!playerName">
+        <form autocomplete="off" @submit.prevent="setPlayerName()">
+          <div class="columns is-mobile is-centered">
+            <div class="field has-addons is-centered">
+              <div class="control">
+                <input class="input" type="text" placeholder="Input player name">
+              </div>
+              <div class="control">
+                <button class="button is-info">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </form> 
+      </div>
+
+      <div v-show="playerName">
+        <div class="columns">
+          <div class="column is-half is-offset-one-quarter">
+            <button v-show="!timerEnabled" class="button is-primary is-fullwidth" @click="setTimer(180)">Start</button>
+          </div>
         </div>
       </div>
-      
+
     </div>
 
     <div v-show="started">
-      
-      <br>
-      
-      <form class="pokemons-form" autocomplete="off" @submit.prevent="onSubmit">
-        <div class="columns is-mobile is-centered">
-          <div class="field has-addons is-centered">
-            <div class="control">
-              <input id="pokemonInput" v-model="pokemonName" class="input" type="text" placeholder="Input a pokemans">
-            </div>
-            <div class="control">
-              <button class="button is-info">
-                Submit
-              </button>
-            </div>
-          </div>
-        </div>
-      </form> 
 
-      <br>
+      <div class="columns">
+        <div class="column is-one-third is-offset-one-third">
+          <p><progress class="progress is-warning" :value="timerCount" max="180"></progress></p>
+          <br>
+          <h2 class="subtitle">{{ timeFormat }} (mutliplier x{{ pointsMultiplier }})</h2>
+        </div>
+      </div>
+      
       <br>
 
       <div class="columns">
-        <div class="column is-half is-one-quarter">
-          <h6 class="title is-6">Correct: {{ Math.round((progress + Number.EPSILON) * 100) / 100 }}%</h6>
+        <div class="column box is-one-third is-offset-one-third has-background-info">
+          <div class="field">
+          <div class="control">
+            <input id="pokemonInput" autocomplete="off" v-model="pokemonName" class="input" type="text" placeholder="type here">
+          </div>
+          <p class="help">Input pokemans</p>
+          </div>
+        </div>
+      </div>
+      
+      <transition name="fade">
+        <p v-show="showTick" class="has-text-success">Correct</p>
+      </transition>
+
+      <br>
+
+      <div class="columns">
+        <div class="column is-one-third is-offset-one-third">
+          <h6 class="title is-6">Correct: {{ Math.round((progress + Number.EPSILON) * 100) / 100 }}% ({{points}} points)</h6>
           <progress class="progress is-primary" :value="correct.length" max="151"></progress>
         </div>
-        <div class="column">
-          <h6 class="title is-6">{{ timeFormat }}</h6>
-          <progress class="progress is-warning" :value="timerCount" max="180"></progress>
-        </div>
-        <div class="column">
-          <h6 class="title is-6">Points</h6>
-          {{ points }}
-        </div>
-        <div class="column">
-          <h6 class="title is-6">Points multiplier</h6>
-          {{ pointsMultiplier }}
+      </div>
+      
+      <br>
+
+      <div class="columns">
+        <div class="column is-one-third is-offset-one-third">
+          <button v-show="timerEnabled" class="button is-one-third is-danger is-fullwidth" @click="stopTimer()">End game</button>
         </div>
       </div>
 
       <hr>
 
       <div v-if="correct.length > 0">
-        <h2 class="subtitle has-text-left is-underlined">Correct</h2>
         <div class="columns is-flex-wrap-wrap">
-          <div v-for="(poke, index) in correct" :key="index" class="column is-one-fifth has-text-primary">{{ poke }}</div>
+          <div v-for="(poke, index) in correct" :key="index" class="column is-one-eigth has-text-primary">{{ poke }}</div>
         </div>
       </div>
 
-      <div v-if="incorrect.length > 0">
-        <hr>
-        <h2 class="subtitle has-text-left is-underlined">Incorrect</h2>
-        <div class="columns is-flex-wrap-wrap">
-          <div v-for="(poke, index) in incorrect" :key="index" class="column is-one-fifth has-text-danger">{{ poke }}</div>
-        </div>
-      </div>
-    
-    </div> 
+    </div>
 
   </div>
 </template>
 
 <script>
+import { useCookies } from "vue3-cookies";
+
 export default {
+   setup() {
+    const { cookies } = useCookies();
+    return { cookies };
+  },
   data() {
     return {
       pokemon: [],
@@ -97,6 +115,10 @@ export default {
       finished: false,
       correct: [],
       incorrect: [],
+      showTick: false,
+      playerName: null,
+
+      tester:'',
 
       // TODO split these to CountDown component
       timerEnabled: false,
@@ -109,20 +131,32 @@ export default {
     }
   },
   methods: {
-
+    setPlayerName(playerName) {
+      this.cookies.set("playerName", playerName);
+      this.playerName = this.cookies.get('playerName');
+    },
     finish() {
       this.finished = true
     },
+    playTransition() {
+      this.showTick = true;
+      setTimeout(() => {
+          if(this.showTick) {
+            console.log('waiting');
+            this.showTick = false;
+          }
+      }, 500)
+    },
     onSubmit() {
-      this.pokemonName = this.pokemonName.toLowerCase().replace('-','').trim();
-      if (this.pokemon.includes(this.pokemonName)) {
-        this.points += this.pointsMultiplier * 1
-        this.correct.push(this.pokemonName)
+      // var formattedName = this.pokemonName.toLowerCase().replace('-','').trim();
+      // var formattedName = this.pokemonName.toLowerCase().trim();
+      var formattedName = this.pokemonName.toLowerCase();
+      if (this.pokemon.includes(formattedName) && !this.correct.includes(formattedName)) {
+        this.points += this.pointsMultiplier * 1;
+        this.correct.push(formattedName);
+        this.pokemonName = '';
+        this.playTransition();
       }
-      else {
-        this.incorrect.push(this.pokemonName)
-      }
-      this.pokemonName = ''
     },
 
     // TODO split these to CountDown component
@@ -133,17 +167,17 @@ export default {
     },
     startTimer() {
       this.started = true;
-        if(this.timerEnabled) {
-            setTimeout(() => {
-                if(this.timerCount) {
-                    this.timerCount -= 1;
-                    this.timeMin = Math.floor(this.timerCount / 60);
-                    this.timeSec = this.timerCount % 60;
-                    this.timeFormat = `${this.timeMin}m ${this.timeSec}s`;
-                    this.startTimer();
-                }
-            }, 1000)
-        }
+      if(this.timerEnabled) {
+        setTimeout(() => {
+            if(this.timerCount) {
+                this.timerCount -= 1;
+                this.timeMin = Math.floor(this.timerCount / 60);
+                this.timeSec = this.timerCount % 60;
+                this.timeFormat = `${this.timeMin}m ${this.timeSec}s`;
+                this.startTimer();
+            }
+        }, 1000)
+      }
     },
     stopTimer() {
         this.timerEnabled = false;
@@ -197,6 +231,16 @@ export default {
     .catch(err => {
         console.log(err);
     });
+
+    if (this.cookies.get("playerName")) {
+      this.playerName = this.cookies.get("playerName");
+    }
+
   },
+  watch: {
+    pokemonName(value) {
+      this.onSubmit();
+    }
+  }
 }
 </script>
