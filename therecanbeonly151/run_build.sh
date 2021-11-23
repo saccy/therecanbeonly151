11e381614c2c963f37224f1d3eb61f7c2ae1f24b
Andrew Hudson <a.j.hudson@hotmail.com>
2021-11-23 13:43:37 +1100

    debug env vars

--- therecanbeonly151/run_build.sh
+++ therecanbeonly151/run_build.sh
@@ -6,10 +6,10 @@
 
 # vue create therecanbeonly151
 
-echo "VITE_JSONBIN_API_KEY=${VITE_JSONBIN_API_KEY}" > .env
+echo "VITE_JSONBIN_API_KEY=${VITE_JSONBIN_API_KEY}" >> .env
 echo "VITE_JSONBIN_COLLECTION_ID=${VITE_JSONBIN_COLLECTION_ID}" >> .env
-echo "VUE_APP_JSONBIN_API_KEY=${VITE_JSONBIN_API_KEY}" > .env
-echo "VUE_APP_JSONBIN_COLLECTION_ID=${VITE_JSONBIN_COLLECTION_ID}" >> .env
+# echo "VUE_APP_JSONBIN_API_KEY=${VITE_JSONBIN_API_KEY}" >> .env
+# echo "VUE_APP_JSONBIN_COLLECTION_ID=${VITE_JSONBIN_COLLECTION_ID}" >> .env
 # sed -i 's/\$\$\$/\$/g' .env
 sed -i 's/\$\$\$/\\\$/g' .env
 npm run build
