2d15eb9482074e0ee512c45958922258af718054
Andrew Hudson <a.j.hudson@hotmail.com>
2021-11-23 13:28:23 +1100

    debug env var escape of special chars

--- therecanbeonly151/run_build.sh
+++ therecanbeonly151/run_build.sh
@@ -6,6 +6,7 @@
 
 # vue create therecanbeonly151
 
-echo $VITE_JSONBIN_API_KEY > .env
-echo $VITE_JSONBIN_COLLECTION_ID >> .env
+echo "VITE_JSONBIN_API_KEY=${VITE_JSONBIN_API_KEY}" > .env
+echo "VITE_JSONBIN_COLLECTION_ID=${VITE_JSONBIN_COLLECTION_ID}" >> .env
+sed -i 's/\$\$\$/\$/g' .env
 npm run build
