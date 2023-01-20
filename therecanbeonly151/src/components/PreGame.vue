418556b876287fc74001dfacf8c411d4221cb521
Andrew Hudson <a.j.hudson@hotmail.com>
2023-01-20 14:10:21 +1100

    Added tip

--- therecanbeonly151/src/components/PreGame.vue
+++ therecanbeonly151/src/components/PreGame.vue
@@ -3,6 +3,7 @@
         <div class="column is-half is-offset-one-quarter">
             <h2 class="subtitle">Gotta name 'em all in <b>3 minutes</b>!</h2>
             <h2 class="subtitle">A dimishing points multiplier is in effect, the quicker you are the better your score.</h2>
+            <h2 class="subtitle">You dont need to press enter! If you type a pokemon name correctly it is automatically added.</h2>
         </div>
         <div  v-show="playerName">
             <h2 class="subtitle"><b>{{ playerName }}</b>, I choose you!</h2>
